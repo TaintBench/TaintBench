583334e46f64e2e51ba7c6672149059732981fc9
AnonymousAnalyst <>
2020-07-03 10:24:03 +0200

    save

--- taintbench_brew_configs/fdold/clear.sh
+++ taintbench_brew_configs/fdold/clear.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo Running clear.sh
-if [ -d "results" ]; then
+if [ -d "results" ]; then 
+    sudo mkdir /home/runner/work/TaintBench/TaintBench/AnalysisTools/FlowDroid_April_2017_Nightly/savedResults
+    sudo cp -a /home/runner/work/TaintBench/TaintBench/AnalysisTools/FlowDroid_April_2017_Nightly/results/. /home/runner/work/TaintBench/TaintBench/AnalysisTools/FlowDroid_April_2017_Nightly/savedResults/
     sudo rm /home/runner/work/TaintBench/TaintBench/AnalysisTools/FlowDroid_April_2017_Nightly/results/*
 fi
