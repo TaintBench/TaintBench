70c4855b024260daaf6ba0707fba72e09c503381
AnonymousAnalyst <>
2020-07-04 00:11:16 +0200

    fix clear

--- taintbench_brew_configs/fd271/clear.sh
+++ taintbench_brew_configs/fd271/clear.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo Running clear.sh
-if [ -d "results" ]; then 
+if [ -d "/home/runner/work/TaintBench/TaintBench/AnalysisTools/FlowDroid_April_2017_Nightly/results" ]; then 
    sudo rm /home/runner/work/TaintBench/TaintBench/AnalysisTools/FlowDroid_2.7.1_Release/results/*
 fi
