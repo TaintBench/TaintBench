8805f9fe5ec6e5e82e4a395550c330ebdd908040
AnonymousAnalyst <>
2020-07-03 15:32:52 +0200

    update

--- taintbench_brew_configs/ad312/aqlRunSS.sh
+++ taintbench_brew_configs/ad312/aqlRunSS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update Sources and Sinks
-if [-d "~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks.txt"]; then 
+if [ -d "~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks.txt" ]; then 
     sudo mv ~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks.txt ~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks_bak.txt
     sudo cp -v ${3} ~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks.txt
 fi
@@ -16,9 +16,9 @@ fi
 java -Xmx${2}g -jar argus-saf_2.12-3.1.2-assembly.jar t -o outputPath ${1}
 
 # Restore Sources and Sinks
-if [-d "~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks.txt"]; then 
+if [ -d "~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks.txt" ]; then 
     sudo rm -f ~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks.txt
 fi 
-if [-d "~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks_bak.txt"]; then 
+if [ -d "~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks_bak.txt" ]; then 
     sudo mv ~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks_bak.txt ~/.amandroid_stash/amandroid/taintAnalysis/sourceAndSinks/TaintSourcesAndSinks.txt
 fi 
